b14aeef31e5f068a4186ea6edf6bdf46537404f6
paul <paulbrow06@gmail.com>
2024-01-17 20:29:57 +0530

    removed

--- 00-products-website/client/src/components/PrivateComponents.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Outlet, Navigate } from "react-router-dom";
-
-const PrivateComponents = () => {
-  let auth = localStorage.getItem("user");
-  return auth ? <Outlet /> : Navigate("/signup");
-};
-
-export default PrivateComponents;
